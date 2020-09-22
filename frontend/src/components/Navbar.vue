dc71093d456b64d01d42dfbe649046122b9eaa07
ChewyJetpack <chewyjetpack@gmail.com>
2020-09-22 11:13:13 +0100

    fixed measurement vars, styles header

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,10 +1,14 @@
 <template>
-  <header class="header">
+  <header class="header u-bottom-spacer-xxl">
       <g-link to="/" class="header__title">
         {{ $static.metadata.siteName }}
       </g-link>
       <nav class="header__nav">
-        <g-link v-for="category in $static.strapi.categories" :key="category.id" class="header__nav-item" :to="`/${category.slug}/`">{{ category.title }}</g-link>
+        <ul class="header__nav-list">
+          <li v-for="category in $static.strapi.categories" :key="category.id" class="header__nav-item">
+            <g-link class="header__nav-link" :to="`/${category.slug}/`">{{ category.title }}</g-link>
+          </li>
+        </ul>
       </nav>
   </header>
 </template>
@@ -23,3 +27,40 @@ query {
   }
 }
 </static-query>
+
+<style lang="scss" scoped>
+  .header {
+    padding: $unit_xl 0 $unit_m;
+    display: flex;
+    align-items: flex-end;
+    border-bottom: solid 1px rgba($slate_2, 0.2);
+
+    &__title {
+      font-size: $txt_xl;
+      line-height: 1em;
+      font-family: $heading_font;
+      font-weight: 700;
+      text-decoration: none;
+      color: $carbon;
+    }
+
+    &__nav {
+      margin-left: auto;
+
+      &-list {
+        display: flex;
+      }
+
+      &-item {
+        margin-left: $unit_l;
+      }
+
+      &-link {
+        text-decoration: none;
+        color: $carbon;
+        font-weight: 400;
+        font-size: $txt_s;
+      }
+    }
+  }
+</style>
