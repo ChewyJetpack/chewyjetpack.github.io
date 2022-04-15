ce59520a1d6807c22c2d93d408cdd7a885717c1c
Chewy <chewy@Chewys-MacBook-Pro.local>
2022-04-15 22:24:47 +0100

    admin

--- static/admin/index.js
+++ static/admin/index.js
@@ -1,1 +1,5 @@
 import CMS from "netlify-cms";
+// Initialize the CMS object
+CMS.init()
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate('my-template', MyTemplate)
