03d013e8fa12dd4c90ebd4a4616b04520081cf4d
Chewy <chewy@Chewys-MacBook-Pro.local>
2022-04-19 09:55:14 +0100

    page structure updates to move posts into a dir, tags updated to 'list'

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -4,10 +4,13 @@
     <h1>Blog</h1>
     <ul>
       <li v-for="post of posts" :key="post.slug">
-        <NuxtLink :to="post.slug">
+        <NuxtLink :to="`posts/${post.slug}`">
           {{ post.title }}
           <img :src="post.hero" :alt="post.title">
         </NuxtLink>
+        <ul>
+          <li v-for="tag of post.tags" :key="tag.slug">{{ tag.name }}</li>
+        </ul>
       </li>
     </ul>
   </div>
