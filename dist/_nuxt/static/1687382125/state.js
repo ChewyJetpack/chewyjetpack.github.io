dc955277121670ee244aaadd11283496445fd3c1
chewyjetpack <emilsmith@mailbox.org>
2023-06-22 10:42:32 +0100

    removed insta, added content spacing

--- dist/_nuxt/static/1687382125/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1687382125",layout:"DefaultLayout",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d0a7c507"}}}}(null,"\u002F"));
--- dist/_nuxt/static/1687382125/tags/music-production/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1687382125",layout:"DefaultLayout",error:a,serverRendered:true,routePath:"\u002Ftags\u002Fmusic-production",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d0a7c507"}}}}(null));
