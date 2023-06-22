d99d79c61ad9da902a476868dc67cf281a422daf
chewyjetpack <emilsmith@mailbox.org>
2023-06-23 00:01:02 +0100

    netlify integration

--- dist/_nuxt/static/1687473914/tags/3d-modelling/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1687473914",layout:"DefaultLayout",error:a,serverRendered:true,routePath:"\u002Ftags\u002F3d-modelling",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d171ef3"}}}}(null));
