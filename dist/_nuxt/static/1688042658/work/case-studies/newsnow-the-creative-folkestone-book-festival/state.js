433522e1af50f546bd6de8bf81bc748db1ab13d2
chewyjetpack <emilsmith@mailbox.org>
2023-06-29 13:44:37 +0100

    fixed broken links

---
+++ dist/_nuxt/static/1688042658/work/case-studies/newsnow-the-creative-folkestone-book-festival/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1688042658",layout:"DefaultLayout",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fcase-studies\u002Fnewsnow-the-creative-folkestone-book-festival",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"11409cb0"}}}}(null));
